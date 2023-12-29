8b441fcd0228ac91a677b93f1cacd98e054e5697
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-29 19:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
+
+  var rows = prompt("Input number of rows");
+  var columns = prompt("Input number of columns");
   
+  var table = document.getElementById("myTable");
+
+  // Clear existing table content if any
+  table.innerHTML = '';
+
+  for (var i = 0; i < rows; i++) {
+    var row = table.insertRow(i);
+    for (var j = 0; j < columns; j++) {
+      var cell = row.insertCell(j);
+      cell.innerHTML = "Row-" + i + " Column-" + j;
+    }
+  }
 }
